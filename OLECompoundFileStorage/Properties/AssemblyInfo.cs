77515fe54807f2eb6a41d571b621c8e412748d75
ironfede <ironfede@99b98b21-fab8-469c-bfc5-76399401e515>
2011-03-22 22:30:18 +0000

    RC2 - fixed zero length stream deletion

--- OLECompoundFileStorage/Properties/AssemblyInfo.cs
+++ OLECompoundFileStorage/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenMcdf (RC1)")]
+[assembly: AssemblyTitle("OpenMcdf (RC2)")]
 [assembly: AssemblyDescription("MS Compound File Storage .NET Implementation")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Federico Blaseotto")]
