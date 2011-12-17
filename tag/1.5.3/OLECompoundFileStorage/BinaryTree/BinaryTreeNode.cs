79bd74fbfa8d84e7063cdb4c6234cdc892081f5e
ironfede <ironfede@99b98b21-fab8-469c-bfc5-76399401e515>
2011-12-17 19:18:14 +0000

    Release 1.5.3

---
+++ tag/1.5.3/OLECompoundFileStorage/BinaryTree/BinaryTreeNode.cs
@@ -0,0 +1,71 @@
+﻿#region Using directives
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace BinaryTrees
+{
+    /// <summary>
+    /// The BinaryTreeNode class represents a node in a binary tree, or a binary search tree.
+    /// It has precisely two neighbors, which can be accessed via the Left and Right properties.
+    /// </summary>
+    /// <typeparam name="T">The type of data stored in the binary tree node.</typeparam>
+    public class BinaryTreeNode<T> : Node<T>
+    {
+        #region Constructors
+        public BinaryTreeNode() : base() {}
+        public BinaryTreeNode(T data) : base(data, null) {}
+        public BinaryTreeNode(T data, BinaryTreeNode<T> left, BinaryTreeNode<T> right)
+        {
+            base.Value = data;
+            NodeList<T> children = new NodeList<T>(2);
+            children[0] = left;
+            children[1] = right;
+
+            base.Neighbors = children;
+        }
+        #endregion
+
+        #region Public Properties
+        public BinaryTreeNode<T> Left
+        {
+            get
+            {
+                if (base.Neighbors == null)
+                    return null;
+                else
+                    return (BinaryTreeNode<T>) base.Neighbors[0];
+            }
+            set
+            {
+                if (base.Neighbors == null)
+                    base.Neighbors = new NodeList<T>(2);
+
+                base.Neighbors[0] = value;
+            }
+        }
+
+        public BinaryTreeNode<T> Right
+        {
+            get
+            {
+                if (base.Neighbors == null)
+                    return null;
+                else
+                    return (BinaryTreeNode<T>) base.Neighbors[1];
+            }
+            set
+            {
+                if (base.Neighbors == null)
+                    base.Neighbors = new NodeList<T>(2);
+
+                base.Neighbors[1] = value;
+            }
+        }
+        #endregion
+    }
+}
