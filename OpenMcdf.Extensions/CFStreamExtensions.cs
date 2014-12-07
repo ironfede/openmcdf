9eee9074189b45bf261c7d740638a76915f4793e
ironfede <ironfede@99b98b21-fab8-469c-bfc5-76399401e515>
2014-12-07 16:50:37 +0000

    Merge from dev branch (2.0 beta 1)

---
+++ OpenMcdf.Extensions/CFStreamExtensions.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace OpenMcdf.Extensions
+{
+    public static class CFStreamExtension
+    {
+        private class StreamDecorator : Stream
+        {
+            private CFStream cfStream;
+            private long position = 0;
+
+            public StreamDecorator(CFStream cfstream)
+            {
+                this.cfStream = cfstream;
+            }
+
+            public override bool CanRead
+            {
+                get { return true; }
+            }
+
+            public override bool CanSeek
+            {
+                get { return true; }
+            }
+
+            public override bool CanWrite
+            {
+                get { return true; }
+            }
+
+            public override void Flush()
+            {
+                // nothing to do;
+            }
+
+            public override long Length
+            {
+                get { return cfStream.Size; }
+            }
+
+            public override long Position
+            {
+                get
+                {
+                    return position;
+                }
+                set
+                {
+                    position = value;
+                }
+            }
+
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                if (count > buffer.Length)
+                    throw new ArgumentException("Count parameter exceeds buffer size");
+
+                if (buffer == null)
+                    throw new ArgumentNullException("Buffer cannot be null");
+
+                if (offset < 0 || count < 0)
+                    throw new ArgumentOutOfRangeException("Offset and Count parameters must be non-negative numbers");
+
+                if (position >= cfStream.Size)
+                    return 0;
+
+                count = this.cfStream.Read(buffer, position, offset, count);
+                position += count;
+                return count;
+            }
+
+            public override long Seek(long offset, SeekOrigin origin)
+            {
+                switch (origin)
+                {
+                    case SeekOrigin.Begin:
+                        position = offset;
+                        break;
+                    case SeekOrigin.Current:
+                        position += offset;
+                        break;
+                    case SeekOrigin.End:
+                        position -= offset;
+                        break;
+                    default:
+                        throw new Exception("Invalid origin selected");
+                }
+
+                return position;
+            }
+
+            public override void SetLength(long value)
+            {
+                this.cfStream.Resize(value);
+            }
+
+            public override void Write(byte[] buffer, int offset, int count)
+            {
+                this.cfStream.Write(buffer, position, offset, count);
+                position += count;
+            }
+
+            public override void Close()
+            {
+                // Do nothing
+            }
+        }
+
+        /// <summary>
+        /// Return the current <see cref="T:OpenMcdf.CFStream">CFStream</see> object 
+        /// as a <see cref="T:System.IO.Stream">Stream</see> object.
+        /// </summary>
+        /// <param name="cfStream">Current <see cref="T:OpenMcdf.CFStream">CFStream</see> object</param>
+        /// <returns>A <see cref="T:System.IO.Stream">Stream</see> object representing structured stream data</returns>
+        public static Stream AsIOStream(this CFStream cfStream)
+        {
+            return new StreamDecorator(cfStream);
+        }
+    }
+}
---
+++ Unit Test/RBTreeTest.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenMcdf;
+using RedBlackTree;
+
+namespace OpenMcdfTest
+{
+    /// <summary>
+    /// Summary description for RBTreeTest
+    /// </summary>
+    [TestClass]
+    public class RBTreeTest
+    {
+        public RBTreeTest()
+        {
+            
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        internal IList<IDirectoryEntry> GetDirectoryRepository(int count)
+        {
+            List<IDirectoryEntry> repo = new List<IDirectoryEntry>();
+            for (int i = 0; i < count; i++)
+            {
+                IDirectoryEntry de =  DirectoryEntry.New(i.ToString(), StgType.StgInvalid, repo);
+            }
+
+            return repo;
+        }
+
+        [TestMethod]
+        public void Test_RBTREE_INSERT()
+        {
+            RBTree rbTree = new RBTree();
+            System.Collections.Generic.IList<IDirectoryEntry> repo = GetDirectoryRepository(25);
+
+            foreach (var item in repo)
+            {
+                rbTree.Insert(item);
+            }
+
+            for (int i = 0; i < repo.Count; i++)
+            {
+                IRBNode c;
+                rbTree.TryLookup(DirectoryEntry.Mock(i.ToString(), StgType.StgInvalid), out c);
+                Assert.IsTrue(c is IDirectoryEntry);
+                Assert.IsTrue(((IDirectoryEntry)c).Name == i.ToString());
+                //Assert.IsTrue(c.IsStream);
+            }
+        }
+
+
+        [TestMethod]
+        public void Test_RBTREE_DELETE()
+        {
+            RBTree rbTree = new RBTree();
+            System.Collections.Generic.IList<IDirectoryEntry> repo = GetDirectoryRepository(25);
+
+
+            foreach (var item in repo)
+            {
+                rbTree.Insert(item);
+            }
+
+            try
+            {
+                IRBNode n;
+                rbTree.Delete(DirectoryEntry.Mock("5", StgType.StgInvalid),out n);
+                rbTree.Delete(DirectoryEntry.Mock("24", StgType.StgInvalid), out n);
+                rbTree.Delete(DirectoryEntry.Mock("7", StgType.StgInvalid), out n);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Item removal failed: " + ex.Message);
+            }
+
+
+
+            //    CFItem c;
+            //    bool s = rbTree.TryLookup(new CFMock("7", StgType.StgStream), out c);
+
+
+            //    Assert.IsFalse(s);
+
+            //    c = null;
+
+            //    Assert.IsTrue(rbTree.TryLookup(new CFMock("6", StgType.StgStream), out c));
+            //    Assert.IsTrue(c.IsStream);
+            //    Assert.IsTrue(rbTree.TryLookup(new CFMock("12", StgType.StgStream), out c));
+            //    Assert.IsTrue(c.Name == "12");
+
+
+            //}
+
+          
+        }
+
+        private static void VerifyProperties(RBTree t)
+        {
+            VerifyProperty1(t.Root);
+            VerifyProperty2(t.Root);
+            // Property 3 is implicit
+            VerifyProperty4(t.Root);
+            VerifyProperty5(t.Root);
+        }
+
+        private static Color NodeColor(IRBNode n) 
+        {
+            return n == null ? Color.BLACK : n.Color;
+        }
+
+        private static void VerifyProperty1(IRBNode n)
+        {
+
+            Assert.IsTrue(NodeColor(n) == Color.RED || NodeColor(n) == Color.BLACK);
+
+            if (n == null) return;
+            VerifyProperty1(n.Left);
+            VerifyProperty1(n.Right);
+        }
+
+        private static void VerifyProperty2(IRBNode root)
+        {
+            Assert.IsTrue(NodeColor(root) == Color.BLACK);
+        }
+
+        private static void VerifyProperty4(IRBNode n) 
+        {
+
+            if (NodeColor(n) == Color.RED)
+            {
+                Assert.IsTrue((NodeColor(n.Left) == Color.BLACK));
+                Assert.IsTrue((NodeColor(n.Right) == Color.BLACK));
+                Assert.IsTrue((NodeColor(n.Parent) == Color.BLACK));
+            }
+
+            if (n == null) return;
+            VerifyProperty4(n.Left);
+            VerifyProperty4(n.Right);
+        }
+
+        private static void VerifyProperty5(IRBNode root)
+        {
+            VerifyProperty5Helper(root, 0, -1);
+        }
+
+        private static int VerifyProperty5Helper(IRBNode n, int blackCount, int pathBlackCount)
+        {
+            if (NodeColor(n) == Color.BLACK)
+            {
+                blackCount++;
+            }
+            if (n == null)
+            {
+                if (pathBlackCount == -1)
+                {
+                    pathBlackCount = blackCount;
+                }
+                else
+                {
+
+                    Assert.IsTrue(blackCount == pathBlackCount);
+
+                }
+                return pathBlackCount;
+            }
+
+            pathBlackCount = VerifyProperty5Helper(n.Left, blackCount, pathBlackCount);
+            pathBlackCount = VerifyProperty5Helper(n.Right, blackCount, pathBlackCount);
+
+            return pathBlackCount;
+        }
+
+
+
+        [TestMethod]
+        public void Test_RBTREE_ENUMERATE()
+        {
+            RBTree rbTree = new RBTree();
+            System.Collections.Generic.IList<IDirectoryEntry> repo = GetDirectoryRepository(10000);
+
+            foreach (var item in repo)
+            {
+                rbTree.Insert(item);
+            }
+
+            VerifyProperties(rbTree);
+            //rbTree.Print();
+        }
+    }
+}
