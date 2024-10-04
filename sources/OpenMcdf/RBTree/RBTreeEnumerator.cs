1ecf0df89f3fdb07c558a50262e0cdf1588dbacf
Jeremy Powell <jeremy@visionaid.com>
2024-10-04 13:42:52 +1300

    Refactor RBTree
    - Remove duplicated code
    - Remove unused code
    - Move types to their own files
    - Simplify tracing

---
+++ sources/OpenMcdf/RBTree/RBTreeEnumerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace RedBlackTree
+{
+    public partial class RBTree
+    {
+        // TODO: Make internal in v3 (can seal in v2 since constructor is internal)
+        public sealed class RBTreeEnumerator : IEnumerator<IRBNode>
+        {
+            private readonly List<IRBNode> list = new();
+            int position = -1;
+
+            internal RBTreeEnumerator(RBTree tree)
+            {
+                tree.VisitTree(item => list.Add(item));
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public IRBNode Current => list[position];
+
+            object IEnumerator.Current => list[position];
+
+            public bool MoveNext()
+            {
+                position++;
+                return position < list.Count;
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+        }
+    }
+}
