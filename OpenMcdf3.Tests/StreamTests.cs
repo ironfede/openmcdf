47d7659410efa150358f60dd2c17d1d2715916e1
Jeremy Powell <jeremy@visionaid.com>
2024-11-08 16:28:09 +1300

    Add reference write read test

--- OpenMcdf3.Tests/StreamTests.cs
+++ OpenMcdf3.Tests/StreamTests.cs
@@ -187,17 +187,91 @@ public sealed class StreamTests
             stream.Write(expectedBuffer, 0, expectedBuffer.Length);
         }
 
-        using (var rootStorage = RootStorage.Open(memoryStream))
+        byte[] actualBuffer = new byte[length];
+        using (var rootStorage = RootStorage.Open(memoryStream, StorageModeFlags.LeaveOpen))
         {
             using CfbStream stream = rootStorage.OpenStream("TestStream");
             rootStorage.Validate();
             Assert.AreEqual(length, stream.Length);
 
-            byte[] actualBuffer = new byte[length];
+            stream.ReadExactly(actualBuffer);
+            CollectionAssert.AreEqual(expectedBuffer, actualBuffer);
+        }
+
+#if WINDOWS
+        using (var rootStorage = StructuredStorage.Storage.Open(memoryStream))
+        {
+            IEnumerable<string> entries = rootStorage.EnumerateEntries();
+            using StructuredStorage.Stream stream = rootStorage.OpenStream("TestStream");
+            Assert.AreEqual(length, stream.Length);
+
+            stream.ReadExactly(actualBuffer);
+            CollectionAssert.AreEqual(expectedBuffer, actualBuffer);
+        }
+#endif
+    }
+
+#if WINDOWS
+    [TestMethod]
+    [DataRow(Version.V3, 0)]
+    [DataRow(Version.V3, 63)]
+    [DataRow(Version.V3, 64)] // Mini-stream sector size
+    [DataRow(Version.V3, 65)]
+    [DataRow(Version.V3, 511)]
+    [DataRow(Version.V3, 512)] // Multiple stream sectors
+    [DataRow(Version.V3, 513)]
+    [DataRow(Version.V3, 4095)]
+    [DataRow(Version.V3, 4096)]
+    [DataRow(Version.V3, 4097)]
+    [DataRow(Version.V3, 128 * 512)] // Multiple FAT sectors
+    [DataRow(Version.V3, 1024 * 4096)] // Multiple FAT sectors
+    [DataRow(Version.V3, 7087616)] // First DIFAT chain
+    [DataRow(Version.V3, 2 * 7087616)] // Long DIFAT chain
+    [DataRow(Version.V4, 0)]
+    [DataRow(Version.V4, 63)]
+    [DataRow(Version.V4, 64)] // Mini-stream sector size
+    [DataRow(Version.V4, 65)]
+    [DataRow(Version.V4, 511)]
+    [DataRow(Version.V4, 512)]
+    [DataRow(Version.V4, 513)]
+    [DataRow(Version.V4, 4095)]
+    [DataRow(Version.V4, 4096)] // Multiple stream sectors
+    [DataRow(Version.V4, 4097)]
+    [DataRow(Version.V4, 1024 * 4096)] // Multiple FAT sectors (1024 * 4096)
+    [DataRow(Version.V4, 7087616 * 4)] // First DIFAT chain
+    [DataRow(Version.V4, 2 * 7087616 * 4)] // Long DIFAT chain
+    public void StructuredStorageWriteThenRead(Version version, int length)
+    {
+        // Fill with bytes equal to their position modulo 256
+        byte[] expectedBuffer = new byte[length];
+        for (int i = 0; i < length; i++)
+            expectedBuffer[i] = (byte)i;
+
+        using MemoryStream memoryStream = new();
+        string fileName = Path.GetTempFileName();
+        File.Delete(fileName);
+
+        using (var rootStorage = StructuredStorage.Storage.Create(fileName, StructuredStorage.StorageModes.AccessReadWrite | StructuredStorage.StorageModes.ShareExclusive, version == Version.V4))
+        {
+            IEnumerable<string> entries = rootStorage.EnumerateEntries();
+            using StructuredStorage.Stream stream = rootStorage.CreateStream("TestStream");
+
+            stream.Write(expectedBuffer, 0, expectedBuffer.Length);
+            Assert.AreEqual(length, stream.Length);
+        }
+
+        byte[] actualBuffer = new byte[length];
+        using (var rootStorage = RootStorage.OpenRead(fileName))
+        {
+            using CfbStream stream = rootStorage.OpenStream("TestStream");
+            rootStorage.Validate();
+            Assert.AreEqual(length, stream.Length);
+
             stream.ReadExactly(actualBuffer);
             CollectionAssert.AreEqual(expectedBuffer, actualBuffer);
         }
     }
+#endif
 
     [TestMethod]
     [DataRow(Version.V3, 0)]
