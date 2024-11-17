e0f2eaa4c60a34f96e19e060953dd1ca925551db
Jeremy Powell <jeremy@visionaid.com>
2024-11-17 21:56:22 +1300

    Improve Header coverage

--- OpenMcdf.Tests/BinaryReaderTests.cs
+++ OpenMcdf.Tests/BinaryReaderTests.cs
@@ -6,17 +6,19 @@ public sealed class BinaryReaderTests
     [TestMethod]
     public void ReadGuid()
     {
-        byte[] bytes = new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10 };
+        byte[] bytes = [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10];
         using MemoryStream stream = new(bytes);
         using CfbBinaryReader reader = new(stream);
         Guid guid = reader.ReadGuid();
         Assert.AreEqual(new Guid(bytes), guid);
+
+        Assert.ThrowsException<EndOfStreamException>(() => reader.ReadGuid());
     }
 
     [TestMethod]
     public void ReadFileTime()
     {
-        byte[] bytes = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+        byte[] bytes = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
         using MemoryStream stream = new(bytes);
         using CfbBinaryReader reader = new(stream);
         DateTime actual = reader.ReadFileTime();
@@ -29,7 +31,39 @@ public sealed class BinaryReaderTests
     public void ReadHeader(string fileName)
     {
         using FileStream stream = File.OpenRead(fileName);
-        using CfbBinaryReader reader = new(stream);
+        using MemoryStream memoryStream = new();
+        stream.CopyAllTo(memoryStream);
+
+        using CfbBinaryReader reader = new(memoryStream);
         Header header = reader.ReadHeader();
+
+        stream.CopyAllTo(memoryStream);
+        memoryStream.WriteByte(1); // Corrupt signature
+        Assert.ThrowsException<FormatException>(() => reader.ReadHeader());
+
+        stream.CopyAllTo(memoryStream);
+        memoryStream.Position = 24;
+        memoryStream.WriteByte(1); // Corrupt CLSID
+        Assert.ThrowsException<FormatException>(() => reader.ReadHeader());
+
+        stream.CopyAllTo(memoryStream);
+        memoryStream.Position = 26;
+        memoryStream.WriteByte(1); // Corrupt Major version
+        Assert.ThrowsException<FormatException>(() => reader.ReadHeader());
+
+        stream.CopyAllTo(memoryStream);
+        memoryStream.Position = 28;
+        memoryStream.WriteByte(1); // Corrupt byte order
+        Assert.ThrowsException<FormatException>(() => reader.ReadHeader());
+
+        stream.CopyAllTo(memoryStream);
+        memoryStream.Position = 32;
+        memoryStream.WriteByte(1); // Corrupt mini sector shift
+        Assert.ThrowsException<FormatException>(() => reader.ReadHeader());
+
+        stream.CopyAllTo(memoryStream);
+        memoryStream.Position = 32;
+        memoryStream.WriteByte(1); // Corrupt mini sector shift
+        Assert.ThrowsException<FormatException>(() => reader.ReadHeader());
     }
 }
