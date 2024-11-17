525cde8523562de490c819ea2deb9ec7629cff03
Jeremy Powell <jeremy@visionaid.com>
2024-11-17 22:06:57 +1300

    Improve EntryInfo coverage

--- OpenMcdf.Tests/EntryInfoTests.cs
+++ OpenMcdf.Tests/EntryInfoTests.cs
@@ -4,13 +4,30 @@
 public sealed class EntryInfoTests
 {
     [TestMethod]
-    [DataRow("MultipleStorage.cfs", 1)]
-    [DataRow("TestStream_v3_0.cfs", 1)]
-    [DataRow("TestStream_v4_0.cfs", 1)]
-    public void EnumerateEntryInfos(string fileName, int count)
+    [DataRow("MultipleStorage.cfs", EntryType.Storage, "MyStorage", "/Root Entry")]
+    [DataRow("TestStream_v3_0.cfs", EntryType.Stream, "TestStream", "/Root Entry")]
+    [DataRow("TestStream_v4_0.cfs", EntryType.Stream, "TestStream", "/Root Entry")]
+    public void EnumerateEntryInfos(string fileName, EntryType type, string name, string path)
     {
         using var rootStorage = RootStorage.OpenRead(fileName);
         IEnumerable<EntryInfo> entries = rootStorage.EnumerateEntries();
-        Assert.AreEqual(count, entries.Count());
+        Assert.AreEqual(1, entries.Count());
+
+        EntryInfo entry = entries.First();
+        Assert.AreEqual(type, entry.Type);
+        Assert.AreEqual(name, entry.Name);
+        Assert.AreEqual(path, entry.Path);
+        Assert.AreEqual(Guid.Empty, entry.CLSID);
+        Assert.AreEqual(0, entry.Length);
+        if (type is EntryType.Storage)
+        {
+            Assert.AreNotEqual(DirectoryEntry.ZeroFileTime, entry.CreationTime);
+            Assert.AreNotEqual(DirectoryEntry.ZeroFileTime, entry.ModifiedTime);
+        }
+        else
+        {
+            Assert.AreEqual(DirectoryEntry.ZeroFileTime, entry.CreationTime);
+            Assert.AreEqual(DirectoryEntry.ZeroFileTime, entry.ModifiedTime);
+        }
     }
 }
