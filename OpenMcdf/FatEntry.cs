a8c039856dacb860ab82c54d96196f4193a31924
Jeremy Powell <jeremy@visionaid.com>
2024-11-18 16:27:21 +1300

    Remove redundant code

--- OpenMcdf/FatEntry.cs
+++ OpenMcdf/FatEntry.cs
@@ -5,8 +5,6 @@
 /// </summary>
 internal record struct FatEntry(uint Index, uint Value)
 {
-    internal static readonly FatEntry Invalid = new(uint.MaxValue, uint.MaxValue);
-
     public readonly bool IsFree => Value == SectorType.Free;
 
     public override readonly string ToString() => $"#{Index}: {Value}";
