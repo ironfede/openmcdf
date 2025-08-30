4266db5a92cccb5d7e8e207d616760272b744058
Jeremy Powell <jeremy@visionaid.com>
2025-08-30 14:33:32 +1200

    Move NodeSelection to its own file
    Classes defined before forms cause resource loading errors

--- StructuredStorageExplorer/MainForm.cs
+++ StructuredStorageExplorer/MainForm.cs
@@ -6,40 +6,11 @@ using StructuredStorageExplorer.Properties;
 using System.Collections;
 using System.ComponentModel;
 using System.Data;
-using System.Globalization;
 using FileFormatException = OpenMcdf.FileFormatException;
 
 // Author Federico Blaseotto
 
 namespace StructuredStorageExplorer;
-
-sealed record class NodeSelection(Storage? Parent, EntryInfo EntryInfo)
-{
-    public string SanitizedFileName
-    {
-        get
-        {
-            // A lot of stream and storage have only non-printable characters.
-            // We need to sanitize filename.
-
-            string sanitizedFileName = string.Empty;
-
-            foreach (char c in EntryInfo.Name)
-            {
-                UnicodeCategory category = char.GetUnicodeCategory(c);
-                if (category is UnicodeCategory.LetterNumber or UnicodeCategory.LowercaseLetter or UnicodeCategory.UppercaseLetter)
-                    sanitizedFileName += c;
-            }
-
-            if (string.IsNullOrEmpty(sanitizedFileName))
-            {
-                sanitizedFileName = "tempFileName";
-            }
-
-            return $"{sanitizedFileName}.bin";
-        }
-    }
-}
 
 /// <summary>
 /// Sample Structured Storage viewer to
