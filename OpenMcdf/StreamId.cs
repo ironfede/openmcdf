100ad5203215333ea7c81c645ebb50a4be0aa31a
Jeremy Powell <jeremy@visionaid.com>
2025-09-02 15:44:07 +1200

    Relax start sector ID validation
    Wine creates storage entries with invalid IDs

--- OpenMcdf/StreamId.cs
+++ OpenMcdf/StreamId.cs
@@ -10,6 +10,7 @@ internal static class StreamId
 
     public static bool IsValid(uint value) => value is <= Maximum or NoStream;
 
-    // EndOfChain is not technically valid according to the CFB specification, but it is used in practice by the reference implementation for directory entry start sectors.
+    // EndOfChain is not technically valid according to the CFB specification, but it is used in practice by the
+    // reference implementation for directory entry start sectors and storage entries in Wine.
     public static bool IsValidInPractice(uint value) => value is <= Maximum or NoStream or SectorType.EndOfChain;
 }
