28e3896355d13b05504e53e15c4003d9c713f5d6
Jeremy Powell <jeremy@visionaid.com>
2025-09-10 10:37:01 +1200

    Add net10.0 TFM

--- StructuredStorageExplorer/Program.cs
+++ StructuredStorageExplorer/Program.cs
@@ -8,6 +8,7 @@ static class Program
     [STAThread]
     static void Main()
     {
+        Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
         Application.Run(new MainForm());
