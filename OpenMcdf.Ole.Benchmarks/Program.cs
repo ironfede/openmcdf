d265aefc6f396accc400e8500befb45ba0f88aa3
Richard Webb <Richard.Webb@BoldonJames.com>
2026-04-19 12:22:00 +0100

    Add a new benchmark project for the Ole project, and start adding some tests

---
+++ OpenMcdf.Ole.Benchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace OpenMcdf.Ole.Benchmarks;
+
+internal sealed class Program
+{
+    private static void Main(string[] args)
+    {
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run();
+    }
+}
